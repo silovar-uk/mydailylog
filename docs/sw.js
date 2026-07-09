60cf484c4dae4a35b96ac231d401c08eec9c5385
silovar-uk <46244124+silovar-uk@users.noreply.github.com>
2026-07-10 00:23:29 +0900

    fix: cache desktop composer override

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'mydailylog-v3';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+const CACHE_NAME = 'mydailylog-v4';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './icon.svg', './desktop-input.css'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
